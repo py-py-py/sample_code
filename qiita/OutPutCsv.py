0fdd1407872e2d2b6816c513cd2f1e6ee14a3b21
py-py-py <37635137+py-py-py@users.noreply.github.com>
2019-06-19 12:57:24 +0900

    create outputcsv.py

---
+++ qiita/OutPutCsv.py
@@ -0,0 +1,18 @@
+import csv
+from datetime import datetime
+
+class OutPutCsv(object):
+
+    def __init__(self, data):
+        self.data = data
+        self.now = datetime.now()
+    
+    def output_csv(self):
+        csv_header = ["タイトル", "URL", "著者"]
+        csv_data = []
+        csv_data.append(csv_header)
+        with open("result.csv", "w", encoding="utf-8_sig") as f:
+            writer = csv.writer(f, lineterminator="\n")
+            for data in self.data:
+                csv_data.append(data)
+            writer.writerows(csv_data)
