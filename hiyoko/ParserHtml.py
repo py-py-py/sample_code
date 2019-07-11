bb7ccb65120c62f18506a1ed25799e2be63c0781
py-py-py <37635137+py-py-py@users.noreply.github.com>
2019-07-11 16:05:25 +0900

    ファイル一件ずつにcsvに吐き出すよう変更

--- hiyoko/ParserHtml.py
+++ hiyoko/ParserHtml.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import csv
 
 
 class ParserHtml(object):
@@ -13,7 +14,7 @@ class ParserHtml(object):
     def insert_tag_check(self) -> dict:
         """
         headタグ内にタグがあるかを確認する
-        :return: dict
+        :return: list
         """
 
         html = ""
@@ -39,3 +40,41 @@ class ParserHtml(object):
             self.out_put.update(result)
 
         return self.out_put
+
+    def insert_tag_check2(self):
+        html = ""
+        with open("result2.csv", "a", encoding="utf-8_sig", newline="") as f:
+            csv_header = [
+                "ファイル名", "headタグ内", "結果"
+            ]
+            writer = csv.writer(f)
+            writer.writerow(csv_header)
+            for file in self.files:
+                open_file_for_beautifulsoup = open(file, "r", encoding="utf-8")
+                html = open_file_for_beautifulsoup.read()
+                open_file_for_beautifulsoup.close()
+                html = BeautifulSoup(html, "html.parser")
+                heads = html.find_all("head")
+                result: dict = {}
+
+                result[file] = {
+                    "file_name": file,
+                    "head_tag_content": "",
+                    "check": ""
+                }
+
+                for head in heads:
+                    result[file]["head_tag_content"] = head
+                    """headタグ内に挿入したタグがあるかを文字列の一致で確認する"""
+                    if "<meta charset" in str(head):
+                        result[file]["check"] = "○"
+                    else:
+                        result[file]["check"] = "×"
+                csv_data = [
+                    result[file]["file_name"],
+                    result[file]["head_tag_content"],
+                    result[file]["check"]
+                ]
+                writer.writerow(csv_data)
+
+
